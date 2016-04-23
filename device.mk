64d0ce4fd15cbda401ce0941da93ac4b5932cec5
Raja.M <Raja1612.M@gmail.com>
2016-04-23 15:49:15 -0400

    Bring up titan with EXODUS-6.0

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 # call the proprietary setup
-$(call inherit-product-if-exists, vendor/motorola/titan/titan-vendor.mk)
+$(call inherit-product, vendor/motorola/titan/titan-vendor.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
